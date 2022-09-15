652e9e782ac8ca276fe035cdbd94839fc08fa837
AlHassanHK <al.hassan.elkady@gmail.com>
2022-09-15 09:57:11 +0200

    User routes, controllers done

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,15 @@
+// get all users
+// get user by id
+// create user
+// update email by id
 const express = require("express");
 
 const userController = require("../controllers/userController");
 
 const router = express.Router();
 
-router.route("/").get(userController.getAllUsers);
+router.route("/").get(userController.getAllUsers).post(userController.createUser);
+router.route("/:id").get(userController.getUserById);
+router.route("/:id/update-email").patch(userController.updateEmailById);
 
+module.exports = router;
